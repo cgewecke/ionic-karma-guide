e031e395ef96d999cfa41877e50f467d8baf236f
Christopher gewecke <christophergewecke@gmail.com>
2016-03-07 11:10:36 -0800

    Add contact directive

--- karma-guide/www/js/addcontact.js
+++ karma-guide/www/js/addcontact.js
@@ -1,59 +1,29 @@
 angular.module('starter')
   .directive("addContact", AddContact);
 
-// @directive: <add-contact user='someUserProfile'></add-contact>
+// @directive: <add-contact contact='someUser'></add-contact>
 // @params: user (the profile object of the user to be added). 
 //
-function AddContact($cordovaContacts, $timeout, $auth){
+function AddContact($cordovaContacts){
     return {
        restrict: 'E',   
        replace: true,
-       scope: {user: '='},
-       template: 
-        
-        '<ion-footer-bar align-title="left" class="bar-energized directive-footer"' +
-                        'ng-show="!contactAdded">' +
-          '<h1 class="title" >' +
-            '<span> Add {{user.firstName}} to contacts</span>' +
-          '</h1>' +
-          '<div class="buttons">' +
-            '<button class="button button-clear button-light icon ion-ios-plus-outline" ng-click="createContact()"></button>' +
-          '</div>' +
-        '</ion-footer-bar>',
-
+       scope: {contact: '='},
+       templateUrl: 'templates/addContact.html',
+      
        link: function(scope, elem, attrs){
 
-          scope.currentUserId = Meteor.user().username; // user.username === LinkedIn profile 'id'
-          scope.contactAdded = hasContact(); // Boolean determines visibility of this directive
+          scope.contactAdded = false; // Boolean determines visibility of this directive
 
-          // @function: hasContact
-          // @return: boolean
-          // Determines if currentUser has already added this profile. 
-          function hasContact(){
-            if (!Meteor.user()) return false;
-
-            var contacts = Meteor.user().profile.contacts;
-            for(var i = 0; i < contacts.length; i++){
-                if (contacts[i] === scope.user.id){
-                  return true;
-                }
-            }
-            return false;
-          }; 
-    
-       		// @function: createContact
-          // Adds profile to native contacts, calls meteor to push this contact id
-          // onto the users list of added contacts
+          // Adds to native contacts
           scope.createContact = function(){
-          
-            var contactInfo ={ "displayName": scope.user.name };
+
+            var contactInfo ={ "displayName": contact.name };
             
-            $cordovaContacts.save(contactInfo).then(function(result) {
-                 
+            $cordovaContacts.save(contactInfo).then(function(result) {     
                 scope.contactAdded = true;
-                Meteor.call('addContact', scope.user.id); 
                     
-            }, function(error) { });    
+            });    
           }
         }
     };
